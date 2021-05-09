863b8281c85a6baed483f8c8f3d8295662c15a6a
ALphaxard <alphaxardgacuuru47@gmail.com>
2021-05-10 00:09:50 +0300

    React router globally 2

--- src/index.js
+++ src/index.js
@@ -6,12 +6,12 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Route } from "react-router-dom"
 
 ReactDOM.render(
-  <React.StrictMode>
-		<Router>
+	<Router>
+		<React.StrictMode>
 			<App />
-	  </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+		</React.StrictMode>
+	</Router>,
+	document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
