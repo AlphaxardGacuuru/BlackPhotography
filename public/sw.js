64a9d7a198327c46f6fd043bb5ae6a05f52fec35
Alphaxard Gacuuru <alphaxardgacuuru47@gmail.com>
2024-04-22 20:56:11 +0300

    Updated images

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // cacheName needs to be changed after any of the assets are changed
-const cacheName = 'BlackPhotographyCache2';
+const cacheName = 'BlackPhotographyCache3';
 
 const cacheAssets = [
 	'style.css'
